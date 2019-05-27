3bbd94de66a8352d74aeecff32e4f47253000c14
neketn1 <neket.apple@icloud.com>
2019-05-27 17:25:42 +0300

    USE COMMAND CD AND IS

---
+++ suzen14.sh
@@ -0,0 +1,37 @@
+/home/suzen # ls
+diary.txt  part1
+/home/suzen # cd part1
+/home/suzen/part1 # ls
+d2FodmFoM
+/home/suzen/part1 # cd d2FodmFoM
+/home/suzen/part1/d2FodmFoM # ls
+/home/suzen/part1/d2FodmFoM # cd -
+/home/suzen/part1
+/home/suzen/part1 # ls
+d2FodmFoM
+/home/suzen/part1 # cd home
+sh: cd: can't cd to home: No such file or directory
+/home/suzen/part1 # cd /home
+/home # ls
+john   suzen
+/home # cd john
+/home/john # ls
+Desktop    Documents
+/home/john # cd Documents
+/home/john/Documents # ls
+part2
+/home/john/Documents # cd part2
+/home/john/Documents/part2 # ls
+WFlV2FpYm
+/home/john/Documents/part2 # cd /home
+/home # ls
+john   suzen
+/home # cd john
+/home/john # ls
+Desktop    Documents
+/home/john # cd Desktop
+/home/john/Desktop # ls
+part3
+/home/john/Desktop # cd part3
+/home/john/Desktop/part3 # ls
+VlaG9vMmIK
