223bb87e4dba7a06d2dfb1716c867368da9cdf16
neketn1 <neket.apple@icloud.com>
2019-05-27 17:27:52 +0300

    mkdir -p

---
+++ suzen19.sh
@@ -0,0 +1,16 @@
+/home/suzen # mkdir -p Documents/projects/lab19/using/bash/commands
+/home/suzen # cd Documents/projects/lab19/using/bash/commands
+/home/suzen/Documents/projects/lab19/using/bash/commands # ls -a
+.   ..
+/home/suzen/Documents/projects/lab19/using/bash/commands # ls -l
+total 0
+/home/suzen/Documents/projects/lab19/using/bash/commands # cd total
+sh: cd: can't cd to total: No such file or directory
+/home/suzen/Documents/projects/lab19/using/bash/commands # ls -all
+total 8
+drwxr-sr-x    2 root     suzen         4096 May 20 19:42 .
+drwxr-sr-x    3 root     suzen         4096 May 20 19:42 ..
+/home/suzen/Documents/projects/lab19/using/bash/commands # cd total 0
+sh: cd: can't cd to total: No such file or directory
+/home/suzen/Documents/projects/lab19/using/bash/commands # ls
+/home/suzen/Documents/projects/lab19/using/bash/commands # ls -a
